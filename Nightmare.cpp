39ca9acbc2f178fb10ae6fa1f4b4ab82fd07ad19
hello <KrzysztofPrawdziwyGoral@wp.pl>
2021-08-23 00:52:49 +0200

    Dodaj pliki projektów.

---
+++ Nightmare.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+//Polish Nightmare
+
+
+#define jeśli if
+#define albo else 
+#define zapetl for 
+#define przełacz switch
+#define wPrzypadku case
+#define przerwaij break
+#define podczas while 
+#define zrób  do
+#define podczas while 
+#define continue kontynułuj
+#define zwroc return 
+#define tutaj * 
+
+
+
+
+
+#define znak char 
+#define liczbaCałkowita int
+#define łancuch string  
+#define niepodpisany unsigned 
+#define podpisany signed 
+#define długi long 
+#define przecinkowy float 
+#define podwójny double 
+#define logiczny bool
+#define próżnia void 
+
+#define dodaj + 
+#define odejmnij - 
+#define monzoz * 
+#define podziel / 
+#define modulo % 
+#define równy == 
+#define różny != 
+#define mniejszy niż <	
+#define większy niż >
+#define mniejszyLubRówny <= 
+#define większyLubRówny >= 
+
+
+
+#define prawda true 
+#define fałsz flase 
+#define pusty null 
+
+
+#define i  &&
+#define lub || 
+
+
+#define rozmaiar sizeof
+
+
+#define wczytaj cin 
+#define wypisz cout 
+#define strumieńBłędów cerr 
+#define strumieńLogów clog 
+
+
+
+#define to (
+#define prawda )
+#define zrób {
+#define KONIEC }
+
+
+
+
+
+
+
+int main()
+{
+    łancuch napis = "";
+   
+
+    jeśli to 1 > 2 prawda zrób
+        wypisz << "Hello World!\n";
+    KONIEC
+        albo
+        zrób
+        wczytaj >> napis;
+    KONIEC
+
+
+        wypisz << napis;
+   
+}
+
